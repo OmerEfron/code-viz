9cec200e9feea1a533fd1be4997fc8fbdca62c4f
Omer Efron <omere@onomagic.com>
2025-04-13 21:03:19 +0300

    init validate function

---
+++ code-to-vis-generator/utils.py
@@ -0,0 +1,14 @@
+# read code, and validate it
+# returns: 
+# {
+#     "valid": true,
+#     "errors": ["error1", "error2"],
+#     "lang": c/c++/python/etc,
+#     "code": code,
+# }
+def validate_code(code):
+    pass
+
+
+
+# construct a json object from the code 
